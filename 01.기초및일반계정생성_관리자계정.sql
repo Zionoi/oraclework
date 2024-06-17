fc3a7a7b02443877c78e7c65146782c53242874c
Zionoi <rladygks2848@naver.com>
2024-06-17 18:44:15 +0900

    .

--- 01.기초및일반계정생성_관리자계정.sql
+++ 01.기초및일반계정생성_관리자계정.sql
@@ -83,3 +83,15 @@ alter session set "_oracle_script" = true;
 create user chun identified by 1234;
 grant resource, connect to chun;
 alter user chun default tablespace users quota unlimited on users;
+
+
+--DDL 사용자 만들기
+alter session set "_oracle_script" = true; 
+create user DDL identified by 1234;
+grant resource, connect to DDL;
+alter user DDL default tablespace users quota unlimited on users;
+
+
+
+
+
